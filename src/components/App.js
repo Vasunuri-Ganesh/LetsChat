959a04059ee9dc11ee66dfd0297bf3852f12d6de
Vasunuri-Ganesh <108877012+Vasunuri-Ganesh@users.noreply.github.com>
2022-07-20 12:01:11 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import "../styles/index.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to create your project!</h2>
+    </div>
+  );
+}
