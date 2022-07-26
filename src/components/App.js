acc325e350d6d1656f816fdb5742c6f8f14178b4
Vasunuri-Ganesh <108877012+Vasunuri-Ganesh@users.noreply.github.com>
2022-07-26 12:59:24 +0530

    display-posts

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "../styles/index.css";
+import { Home } from "../pages";
 import { getPosts } from "../api";
+import { Loader } from "./";
 
 export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await getPosts();
       console.log("response", response);
+      if (response.success) {
+        setPosts(response.data.posts);
+      }
+      setLoading(false);
     };
     fetchPosts();
   }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
   return (
     <div className="App">
-      <h1>Hello Sandbox</h1>
-      <h2>Start editing to create your project!</h2>
+      <Home posts={posts} />
     </div>
   );
 }
